6f7416128d9a15f0c4781e334b8b4574a91ff5cd
ravi230195 <shouryam@buffalo.edu>
2020-07-08 10:04:53 -0400

    layout design and dynamic ingredients completed

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,25 @@
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.css';
+import React from 'react';
+const burger = (props)=>{
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(igkey => {return [...Array(props.ingredients[igkey])].map((_,i)=>{
+    return <BurgerIngredient key={igkey+i} type={igkey} />});
+    })
+    .reduce((arr,el)=>{
+        return arr.concat(el)
+    },[]);
+    if(transformedIngredients.length===0){
+        transformedIngredients=<p>Please start adding ingredients!</p>
+    }
+    //console.log(transformedIngredients);
+     return(
+        <div className={classes.Burger}>
+        <BurgerIngredient type="bread-top"/>
+        {transformedIngredients}        
+        <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+};
+
+export default burger;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React,{ Component } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients:{
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    }
+    render (){
+        return(
+            <Auxiliary>
+                <Burger ingredients={this.state.ingredients}/>
+                <div>Burger Controller</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
