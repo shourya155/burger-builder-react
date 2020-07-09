8e4e02d392411b1b12c4add4d3e2e49e8021d780
ravi230195 <shouryam@buffalo.edu>
2020-07-09 15:57:22 -0400

    fixed to fit to mobile screen

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) =>(
+class Layout extends Component{
+    state={
+        showSideDrawer:false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer:false});
+    }
+
+    sideDrawerToggleHandler = () =>{
+        this.setState((prevState)=>{
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+    
+    render(){
+        return(
         <Auxiliary>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar drawerToggleClicked = {this.sideDrawerToggleHandler}/>
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
         <main className={classes.Content}>
-            {props.children}
+            {this.props.children}
         </main>
         </Auxiliary>
-);
+        )
+    }
+}
 
-export default layout;
+
+export default Layout;
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,28 @@
+import React,{ Component} from 'react';
+import classes from './Modal.css';
+import Backdrop from '../Bakdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
+
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show;
+    }
+
+    render(){
+    return(
+        <Auxiliary>
+            <Backdrop clicked={this.props.modalClosed} show={this.props.show}></Backdrop>
+            <div 
+            className={classes.Modal}
+            style={{
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
+                }}
+            >
+            {this.props.children}
+            </div>
+        </Auxiliary>
+        );
+    }
+}
+
+export default Modal;
